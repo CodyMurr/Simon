036ac87ad125c2ab775c304731a5190e3a616760
CodyMurray-ga <codakmurraki@gmail.com>
2022-01-05 15:50:08 -0600

    Fix player/computer turn cycle

--- js/main.js
+++ js/main.js
@@ -1,34 +1,82 @@
-let on, playerSeq, computerSeq, round, lose;
+const board = [
+  'green', 'red', 'yellow', 'blue'
+];
+let round = 0;
+let on, playerSeq, computerSeq, playerTurn, lose;
 
 const power = document.getElementById('toggle');
 const counter = document.getElementById('counter');
-const boardEls = document.querySelectorAll('#board > button');
+const boardEls = [...document.querySelectorAll('#board > button')];
 
-power.addEventListener("click", () => {
-  if (!on) {
-    power.style.justifyContent = 'flex-end';
-    counter.classList = 'on';
-    on = true;
-  } else {
-    power.style.justifyContent = 'flex-start';
-    counter.classList = 'off';
-    counter.innerHTML = '';
-    on = false;
-  }
+power.addEventListener("click", togglePower);
+
+document.querySelector('#start').addEventListener("click", () => {
+  if (!on) return;
+  init();
 });
 
-document.querySelector('#progress button').addEventListener("click", () => {
-  if (!on) return;
+function togglePower() {
+  if (!on) {
+    on = true;
+    renderOn();
+  } else {
+    on = false;
+    renderOff();
+  }
+}
+
+function renderOn() {
+  power.style.justifyContent = 'flex-end';
+  counter.style.backgroundColor = '#940000';
+  counter.style.color = '#ff1947';
+}
+
+function renderOff() {
+  power.style.justifyContent = 'flex-start';
+  counter.style.backgroundColor = '#3f3f3f';
+  counter.innerHTML = '';
+}
+
+boardEls.forEach (btn => {
+  btn.addEventListener("click", (e) => {
+    if (!on || !playerTurn) return;
+    const button = e.target;
+    playerSeq.push(button.id);
+    button.classList.add('pressed');
+    setTimeout(() => {
+      button.classList.remove('pressed');
+    }, 250);
+  });
+  render();
+});
+
+function init() {
   playerSeq = [];
   computerSeq = [];
   round = 1;
   lose = false;
+  getSequence();
   render();
-});
+}
+
+function getSequence() {
+  playerTurn = false;
+  const randomColor = board[Math.floor(Math.random() * board.length)];
+  computerSeq.push(randomColor);
+  renderBoard();
+  render();
+}
 
 function render() {
-  counter.innerHTML = `${round}`;
-  boardEls.forEach(btn => {
-    btn.classList = 'pressed';
+  counter.innerHTML = round;
+}
+
+function renderBoard() {
+  computerSeq.forEach(color => {
+    document.getElementById(color).classList.add('pressed');
+    setTimeout(() => {
+      document.getElementById(color).classList.remove('pressed');
+    }, 250);
   });
+  playerTurn = true;
 }
