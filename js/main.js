316c2400ff360d6384da01bff1c3f83ed8d36a6c
CodyMurray-ga <codakmurraki@gmail.com>
2022-01-06 16:17:03 -0600

    Add win/lose function

--- js/main.js
+++ js/main.js
@@ -1,88 +1,122 @@
-const board = [
-  'green', 'red', 'yellow', 'blue'
-];
-let round = 0;
-let on, computerSeq, playerTurn, lose;
+const BASE_LIT_TIME = 1000;
+const GAP_TIME = 500;
+const MAX_ROUNDS = 10;
+const LEVEL_JUMP = 4;
+const LEVEL_DEC_TIME = 300;
+const board = ['green', 'red', 'yellow', 'blue'];
 
-const power = document.getElementById('toggle');
-const counter = document.getElementById('counter');
-const boardEls = [...document.querySelectorAll('#board > button')];
+let LIT_TIME;
+let compSeq;
+let playerSeq;
+let compTurn;
+let intervalId;
+let wrong;
+let on;
+let win;
 
-power.addEventListener("click", togglePower);
+const colors = [...document.querySelectorAll('#board button')];
+const toggle = document.querySelector('#toggle');
+const onBtn = document.querySelector('#toggle > #power');
+const startBtn = document.querySelector('#start');
 
-document.querySelector('#start').addEventListener("click", () => {
-  if (!on) return;
-  init();
+onBtn.addEventListener("click", (evt) => {
+  if (onBtn.checked === true) {
+    console.log("powered on");
+    init();
+    on = true;
+    toggle.style.justifyContent = 'flex-end';
+    counter.classList.add('on');
+    counter.innerHTML = '-';
+  } else {
+    console.log("powered off");
+    on = false;
+    toggle.style.justifyContent = 'flex-start';
+    counter.classList.remove('on');
+    counter.innerHTML = '';
+  }
 });
 
-function togglePower() {
-  if (!on) {
-    on = true;
-    renderOn();
+startBtn.addEventListener("click", (evt) => {
+  if (on || win) play();
+});
+
+colors.forEach(color => {
+  color.addEventListener("click", handlePlayerSeq);
+});
+
+
+function handlePlayerSeq(e) {
+  if (!on || compTurn) return;
+  const targetIdx = colors.indexOf(e.target);
+  if (targetIdx === -1) return;
+  // add audio
+  playerSeq.push(targetIdx);
+  colors[targetIdx].style.opacity = '100%';
+  setTimeout(() => {
+    if (!wrong) colors[targetIdx].style.opacity = '50%';
+  }, 250);
+  if (checkComplete()) {
+    getCompSeq();
   } else {
-    on = false;
-    renderOff();
+    checkCurrent();
   }
 }
 
-function renderOn() {
-  power.style.justifyContent = 'flex-end';
-  counter.style.backgroundColor = '#940000';
-  counter.style.color = '#ff1947';
-}
-
-function renderOff() {
-  power.style.justifyContent = 'flex-start';
-  counter.style.backgroundColor = '#3f3f3f';
-  counter.innerHTML = '';
-}
-
-boardEls.forEach (btn => {
-  btn.addEventListener("click", checkSequence);
-});
-
-function init() {
-  computerSeq = [];
-  round = 1;
-  lose = false;
-  render();
-}
-
-function getSequence() {
-  playerTurn = false;
-  const randomColor = board[Math.floor(Math.random() * board.length)];
-  computerSeq.push(randomColor);
-  renderBoard();
+function handleSequence() {
+  LIT_TIME = BASE_LIT_TIME - Math.floor(compSeq.length / LEVEL_JUMP) * LEVEL_DEC_TIME;
   render();
 }
 
 function render() {
-  counter.innerHTML = round;
-  getSequence();
-}
-
-function renderBoard() {
-  computerSeq.forEach(color => {
-    document.getElementById(color).classList.add('pressed');
-    setTimeout(() => {
-      document.getElementById(color).classList.remove('pressed');
-    }, 250);
-  });
-  playerTurn = true;
-  checkSequence(e);
-}
-
-function checkSequence(e) {
-  for (let i = 0; i < computerSeq.length; i++) {
-    if (!on || !playerTurn) return;
-    const button = e.target;
-    button.classList.add('pressed');
-    setTimeout(() => {
-      button.classList.remove('pressed');
-    }, 250);
-    if (button.id === computerSeq[i]) {
-      round++;
-      render();
+  let index = 0;
+  intervalId = setInterval(() => {
+    if (!on) {
+      clearInterval(intervalId);
+      return;
     }
+    const color = colors[compSeq[index]];
+    color.style.opacity = '100%';
+    // add audio
+    setTimeout(() => {
+      color.style.opacity = '50%'
+    }, LIT_TIME);
+    index++;
+    if (index >= compSeq.length) {
+      clearInterval(intervalId);
+      compTurn = false;
+    }
+  }, LIT_TIME + GAP_TIME);
+}
+
+function init() {
+  compSeq = [];
+  wrong = false;
+  win = false;
+}
+
+function play() {
+  getCompSeq();
+}
+
+function gameOver() {
+  wrong = true;
+  counter.innerHTML = '!!!'
+}
+
+function getCompSeq() {
+  compTurn = true;
+  playerSeq = [];
+  compSeq.push(Math.floor(Math.random() * board.length));
+  counter.innerHTML = compSeq.length;
+  handleSequence();
+}
+
+function checkComplete() {
+  return JSON.stringify(playerSeq) === JSON.stringify(compSeq);
+}
+
+function checkCurrent() {
+  for (let i = 0; i < playerSeq.length; i++) {
+    playerSeq[i] === compSeq[i] ? true : gameOver();
   }
 }
